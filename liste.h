8c20cb9dd98f3ebde2117fa0f6c498d952d36968
Benjamin <benjamin.joannard@efrei.net>
2023-10-25 11:49:56 +0200

    Correction du fichier liste.h et des struct sk cell et sk list

--- liste.h
+++ liste.h
@@ -5,17 +5,15 @@
 #ifndef AGENDA_LISTE_H
 #define AGENDA_LISTE_H
 
-typedef struct s_d_cell
-{
+typedef struct s_sk_cell {
     int value;
-    struct s_d_cell *next_0;
-    struct s_d_cell *next_1;
-} t_d_cell;
+    int level;
+    strut s_sk_cell **next;
+} t_sk_cell;
 
-typedef struct s_d_list
-{
-    t_d_cell *head_0;
-    t_d_cell *head_1;
-} t_d_list;
+typedef struct s_sk_list {
+    t_sk_cell **heads;
+    int max_level;
+} t_sk_list;
 
 #endif //AGENDA_LISTE_H
